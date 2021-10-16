86d848c1e6c4f24ab6678ab2e21e91658fedc639
Ramana R. A <ramana.prep@gmail.com>
2021-10-16 16:07:34 +0530

    added Aspects @before and @after

--- src/main/java/com/spring/aop/basics/springaop/business/Business1.java
+++ src/main/java/com/spring/aop/basics/springaop/business/Business1.java
@@ -1,5 +1,7 @@
 package com.spring.aop.basics.springaop.business;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +10,13 @@ import com.spring.aop.basics.springaop.data.Dao1;
 @Service
 public class Business1 {
 
+	private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private Dao1 dao1;
 
 	public String CalculateSomething() {
 
+		LOGGER.info("Inside Business1");
 		return dao1.retriveSomeValue();
 	}
 
--- src/main/java/com/spring/aop/basics/springaop/data/Dao1.java
+++ src/main/java/com/spring/aop/basics/springaop/data/Dao1.java
@@ -1,12 +1,17 @@
 package com.spring.aop.basics.springaop.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class Dao1 {
-
+ 
+	private Logger LOGGER  = LoggerFactory.getLogger(this.getClass());
+	
 	public String retriveSomeValue() {
 
+		LOGGER.info("within Dao1");
 		return "Dao1";
 	}
 }
